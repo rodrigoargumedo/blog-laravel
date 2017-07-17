11ac51de290ff6dcaa7f3e9be05c5a949f02dedd
Rodrigo Argumedo <rodrigo.argumedo@outlook.com>
2017-07-17 02:31:28 -0500

    Update the Posts actions, views, and routes.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,9 +2,22 @@
 
 namespace Blog\Http\Controllers;
 
+use Blog\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    // GET /posts/
+    public function index()
+    {
+      $posts = Post::all();
+
+      return view('posts.index', compact('posts'));
+    }
+
+    public function getPost(Post $post) // or function show($id)
+    {
+      return $post;
+      return view('posts.shoq', compact('post'));
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts', 'PostController@index');
+Route::get('/posts/{post}', 'PostController@getPost');
